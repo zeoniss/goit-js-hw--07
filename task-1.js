81b6dc8ba070bff317e2e36d6cc22cf81eb15d0c
Vladimir Kriukov <zeoniss28@gmail.com>
2020-12-17 22:26:20 +0200

    update

--- task-1.js
+++ task-1.js
@@ -1,2 +1,20 @@
-const allItems = document.querySelectorAll('li.item');
-console.log(allItems);
+const allItems = [...document.querySelectorAll('li.item')];
+console.log(`В списке ${allItems.length} категории`);
+
+allItems
+    .map(item => {
+        const titleEl = item.querySelector('h2');
+        const listEl = item.querySelectorAll('ul li');
+        return {
+            titleEl: titleEl.textContent,
+            result: listEl.length,
+        };
+    })
+    .forEach(entry => {
+        console.log(
+            'Категория: ',
+            entry.title,
+            '\r\nКоличество элементов:',
+            entry.result
+        );
+    });
---
+++ task-2.js
@@ -0,0 +1,16 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+];
+const ingredientArray = ingredients.map(menu => {
+    const itemElem = document.createElement('li');
+    itemElem.textContent = menu;
+    return itemElem;
+});
+
+const ingrElem = document.querySelector('#ingredients');
+ingrElem.append(...ingredientArray);
