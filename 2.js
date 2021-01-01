0afb4989ef099cb50d899f6a938f3de7a2892d91
Vladimir Kriukov <zeoniss28@gmail.com>
2021-01-01 16:12:55 +0200

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,23 +1,30 @@
-const orders = [
-    { email: 'solomon@topmail.ua', dish: 'Burger' },
-    { email: 'artemis@coldmail.net', dish: 'Pizza' },
-    { email: 'jacob@mail.com', dish: 'Taco' },
-]
+const service = {
+    mailingList: ["mango@mail.com", "poly@hotmail.de", "ajax@jmail.net"],
+    subscribe(email) {
+        this.mailingList.push(email);
+        return `Почта ${email} добавлена в рассылку.`;
+    },
+    unsubscribe(email) {
+        this.mailingList = this.mailingList.filter(e => e !== email);
+        return `Почта ${email} удалена из рассылки.`;
+    }
+};
 
-// Пиши код ниже этой строки
-const orders = [
-    { email: 'solomon@topmail.ua', dish: 'Burger' },
-    { email: 'artemis@coldmail.net', dish: 'Pizza' },
-    { email: 'jacob@mail.com', dish: 'Taco' },
-]
-
-// Пиши код ниже этой строки
-function composeMessage(position) {
-    return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`
+function logAndInvokeAction(email, action) {
+    console.log(`Выполняем действие с ${email}.`);
+    return action(email);
 }
+logAndInvokeAction(service.subscribe.bind(service));
+console.log(logAndInvokeAction("kiwi@mail.uk", this.subscribe));
+// Почта kiwi@mail.uk добавлена в рассылку.
 
-const messages = orders.map(
-    composeMessage.call((order, position) => {
-        return order, position + 1
-    }),
-)
+console.log(this.mailingList);
+/* ['mango@mail.com', 
+    'poly@hotmail.de', 
+    'ajax@jmail.net', 
+    'kiwi@mail.uk']*/
+
+console.log(logAndInvokeAction("poly@hotmail.de", this.unsubscribe));
+// Почта poly@hotmail.de удалена из рассылки.
+
+console.log(this.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
