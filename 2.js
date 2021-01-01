411ad7fc6b0df9c3a1a990d65ea0166bb751466f
Vladimir Kriukov <zeoniss28@gmail.com>
2021-01-01 16:11:28 +0200

    summary

--- 2.js
+++ 2.js
@@ -1,27 +1,23 @@
-const pizzaPalace = {
-        pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
-        order(pizzaName, onSuccess, onError) {
-            if (this.pizzas.includes(pizzaName)) {
-                return onSuccess(`${pizzaName}`);
-            }
-            return onError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
-        },
-    },
-};
-// Пиши код выше этой строки
+const orders = [
+    { email: 'solomon@topmail.ua', dish: 'Burger' },
+    { email: 'artemis@coldmail.net', dish: 'Pizza' },
+    { email: 'jacob@mail.com', dish: 'Taco' },
+]
 
-// Колбэк для onSuccess
-function makePizza(pizzaName) {
-    return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+// Пиши код ниже этой строки
+const orders = [
+    { email: 'solomon@topmail.ua', dish: 'Burger' },
+    { email: 'artemis@coldmail.net', dish: 'Pizza' },
+    { email: 'jacob@mail.com', dish: 'Taco' },
+]
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+    return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`
 }
 
-// Колбэк для onError
-function onOrderError(error) {
-    return `Ошибка! ${error}`;
-}
-
-// Вызовы метода с колбэками
-pizzaPalace.order('Аль Копчино', makePizza, onOrderError);
-pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
-pizzaPalace.order('Биг майк', makePizza, onOrderError);
-pizzaPalace.order('Венская', makePizza, onOrderError);
+const messages = orders.map(
+    composeMessage.call((order, position) => {
+        return order, position + 1
+    }),
+)
