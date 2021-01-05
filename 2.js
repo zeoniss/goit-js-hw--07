97facc971ad2d70498d2f092242f7833d3cdd3dc
Vladimir Kriukov <zeoniss28@gmail.com>
2021-01-05 17:35:44 +0200

    summary

--- 2.js
+++ 2.js
@@ -1,30 +1,98 @@
-const service = {
-    mailingList: ["mango@mail.com", "poly@hotmail.de", "ajax@jmail.net"],
-    subscribe(email) {
-        this.mailingList.push(email);
-        return `Почта ${email} добавлена в рассылку.`;
-    },
-    unsubscribe(email) {
-        this.mailingList = this.mailingList.filter(e => e !== email);
-        return `Почта ${email} удалена из рассылки.`;
-    }
-};
+// const users = [{
+//         name: "Moore Hensley",
+//         email: "moorehensley@indexia.com",
+//         eyeColor: "blue",
+//         friends: ["Sharron Pace"],
+//         isActive: false,
+//         balance: 2811,
+//         gender: "male",
+//         age: 37
+//     },
+//     {
+//         name: "Sharlene Bush",
+//         email: "sharlenebush@tubesys.com",
+//         eyeColor: "blue",
+//         friends: ["Briana Decker", "Sharron Pace"],
+//         isActive: true,
+//         balance: 3821,
+//         gender: "female",
+//         age: 34
+//     },
+//     {
+//         name: "Ross Vazquez",
+//         email: "rossvazquez@xinware.com",
+//         eyeColor: "green",
+//         friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//         isActive: false,
+//         balance: 3793,
+//         gender: "male",
+//         age: 24
+//     },
+//     {
+//         name: "Elma Head",
+//         email: "elmahead@omatom.com",
+//         eyeColor: "green",
+//         friends: ["Goldie Gentry", "Aisha Tran"],
+//         isActive: true,
+//         balance: 2278,
+//         gender: "female",
+//         age: 21
+//     },
+//     {
+//         name: "Carey Barr",
+//         email: "careybarr@nurali.com",
+//         eyeColor: "blue",
+//         friends: ["Jordan Sampson", "Eddie Strong"],
+//         isActive: true,
+//         balance: 3951,
+//         gender: "male",
+//         age: 27
+//     },
+//     {
+//         name: "Blackburn Dotson",
+//         email: "blackburndotson@furnigeer.com",
+//         eyeColor: "brown",
+//         friends: ["Jacklyn Lucas", "Linda Chapman"],
+//         isActive: false,
+//         balance: 1498,
+//         gender: "male",
+//         age: 38
+//     },
+//     {
+//         name: "Sheree Anthony",
+//         email: "shereeanthony@kog.com",
+//         eyeColor: "brown",
+//         friends: ["Goldie Gentry", "Briana Decker"],
+//         isActive: true,
+//         balance: 2764,
+//         gender: "female",
+//         age: 39
+//     }
+// ];
+// const getTotalFriendCount = users =>
+//     Object.values(users.friends).reduce((total, index) => total + index, 0);
+// ------------------------------------------------------------------------
+// const players = [
+//     { name: "Манго", playtime: 1270, gamesPlayed: 4 },
+//     { name: "Поли", playtime: 469, gamesPlayed: 2 },
+//     { name: "Аякс", playtime: 690, gamesPlayed: 3 },
+//     { name: "Киви", playtime: 241, gamesPlayed: 1 }
+// ];
+// // Пиши код ниже этой строки
+// const averageTimePerGame = players.map(
+//     player => player.playtime / player.gamesPlayed
+// );
 
-function logAndInvokeAction(email, action) {
-    console.log(`Выполняем действие с ${email}.`);
-    return action(email);
-}
-logAndInvokeAction(service.subscribe.bind(service));
-console.log(logAndInvokeAction("kiwi@mail.uk", this.subscribe));
-// Почта kiwi@mail.uk добавлена в рассылку.
-
-console.log(this.mailingList);
-/* ['mango@mail.com', 
-    'poly@hotmail.de', 
-    'ajax@jmail.net', 
-    'kiwi@mail.uk']*/
-
-console.log(logAndInvokeAction("poly@hotmail.de", this.unsubscribe));
-// Почта poly@hotmail.de удалена из рассылки.
-
-console.log(this.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
+// console.log(averageTimePerGame);
+const authors = [
+    'Ли Танит',
+    'Бернард Корнуэлл',
+    'Роберт Шекли',
+    'Федор Достоевский',
+    'Говард Лавкрафт'
+  ];
+  // Пиши код ниже этой строки
+  
+  const authorsInAlphabetOrder = [...authors].sort((a, b) => a.localeCompare(b));
+  
+  const authorsInReversedOrder = [...authors].sort((b, a) => b.localeCompare(a));
